3f490c386dfbad5cde1255ef385ad77614814b79
Tito Merino <tiaguilar@stefanini.com>
2026-01-22 08:31:28 -0600

    [ADD] Creacion de usuario por defecto

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,6 @@ set -e
 
 echo "⏳ Esperando a la base de datos..."
 
-# Railway siempre provee DATABASE_URL
 if [ -n "$DATABASE_URL" ]; then
   echo "🔗 Usando DATABASE_URL"
   until pg_isready -d "$DATABASE_URL"; do
@@ -18,11 +17,32 @@ fi
 
 echo "✅ Base de datos lista"
 
+echo "📦 Recolectando archivos estáticos..."
+python manage.py collectstatic --noinput
+
 echo "📦 Ejecutando migraciones..."
 python manage.py migrate --noinput
 
-echo "🎨 Recolectando archivos estáticos..."
-python manage.py collectstatic --noinput
+echo "👑 Creando superuser si no existe..."
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+import os
+
+User = get_user_model()
+
+username = os.getenv("DJANGO_SUPERUSER_USERNAME")
+email = os.getenv("DJANGO_SUPERUSER_EMAIL")
+password = os.getenv("DJANGO_SUPERUSER_PASSWORD")
+
+if username and password:
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username, email, password)
+        print("✅ Superuser creado")
+    else:
+        print("ℹ️ Superuser ya existe")
+else:
+    print("⚠️ Variables de superuser no definidas, saltando...")
+EOF
 
 echo "🚀 Iniciando Gunicorn..."
 exec gunicorn parkopsbackend.wsgi:application --bind 0.0.0.0:8000

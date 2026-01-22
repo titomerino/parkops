c8dd8f31bc04eea490d73414c942d4539f5937c0
Tito Merino <tiaguilar@stefanini.com>
2026-01-22 17:31:53 -0600

    [ADD] Login

--- shell/urls.py
+++ shell/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import dashboard
+from . import views
 
 urlpatterns = [
     path('', dashboard, name='dashboard'),
+    path('login/', views.custom_login, name='login'),
+    path('logout/', views.custom_logout, name='logout'),
 ]
